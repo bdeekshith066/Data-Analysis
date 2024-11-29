101805cd52561e1beae245d34640b5df07a90bd5
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-11-29 10:11:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 st.set_page_config(layout="wide", page_title="DATA ANALYSIS",
         page_icon="📈📊",)
 
-import home , analysis , operations, introduction
+import home , analysis , operations, introduction, Feature_Engineering, Skewness_OutlierDetection
 
 
 
@@ -52,7 +52,7 @@ class MultiApp:
             
             app = option_menu(
                 menu_title='Sections',
-                options=['Home','Introduction','Operation','Analysis📊📈'],
+                options=['Home','Introduction','Operation','Analysis📊📈','Feature Engineering','Skewness, Outliers, Kurtosis'],
                 default_index=0,
             )
             
@@ -60,7 +60,7 @@ class MultiApp:
             st.sidebar.write("")
             linkedin_url = "https://www.linkedin.com/in/deekshith2912/"
             linkedin_link = f"[ByteBuddies]({linkedin_url})"
-            st.sidebar.subheader(f"Developed  by Deekshith B , Madhurika Priya")
+            st.sidebar.subheader(f"Developed  by Deekshith B , Madhurika Priya, Dinesh")
             
         if app == "Home":
             home.app()
@@ -70,6 +70,10 @@ class MultiApp:
             operations.app() 
         elif app == "Analysis📊📈":
             analysis.app() 
+        elif app == "Feature Engineering":
+            Feature_Engineering.app() 
+        elif app == "Skewness, Outliers, Kurtosis":
+            Skewness_OutlierDetection.app()
        
         
            
